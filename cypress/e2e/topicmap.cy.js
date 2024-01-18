4b76b83f0dabf5d1961668043c7ccd8cf706e09c
Pavel <p-olhovoi@bk.ru>
2024-01-18 21:40:10 +0100

    Check menu and zoom after loading

--- cypress/e2e/topicmap.cy.js
+++ cypress/e2e/topicmap.cy.js
@@ -27,6 +27,7 @@ describe("Smoke test with problems", () => {
     cy.visit(
       "https://topicmaps-wuppertal.github.io/stadtplan/#/?lat=51.25597263790461&lng=7.191198646368561&zoom=8"
     );
+
     cy.get(".leaflet-marker-icon", { timeout: 250000 }).should(
       "have.length.greaterThan",
       100
@@ -34,24 +35,22 @@ describe("Smoke test with problems", () => {
 
     cy.get(".leaflet-marker-icon").first().click();
 
-    cy.url().then((url) => {
-      // Используем регулярное выражение для извлечения значения "zoom"
-      const zoomMatch = url.match(/&zoom=(\d+)/);
+    cy.url().should("include", "&zoom=9");
 
-      // Проверяем, было ли совпадение
-      if (zoomMatch) {
-        // Получаем значение "zoom"
-        const zoom = zoomMatch[1];
+    cy.get(".leaflet-control-zoom-out").click();
+    cy.url().should("include", "&zoom=8");
 
-        // Выводим значение "zoom" в консоль лог
-        cy.log("Zoom: " + zoom);
+    cy.get(".leaflet-control-zoom-in").click();
+    cy.url().should("include", "&zoom=9");
 
-        // Также можно получить отдельную переменную для значения
-        const zoomvalue = parseInt(zoom, 10);
-        cy.log("Zoom as number: " + zoomvalue);
-      } else {
-        cy.log("Zoom not found in the URL");
-      }
-    });
+    cy.get(".modal-content", { timeout: 10000 }).should("not.not.exist");
+
+    cy.get("#cmdShowModalApplicationMenu").should("be.visible").click();
+
+    cy.get(".modal-content", { timeout: 10000 }).should("be.visible");
+
+    cy.get("#cmdCloseModalApplicationMenu").click({ force: true });
+
+    cy.get(".modal-content", { timeout: 10000 }).should("not.not.exist");
   });
 });
