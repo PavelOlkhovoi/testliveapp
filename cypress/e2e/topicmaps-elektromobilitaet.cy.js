fa85fa8fea33572d4edf91ea6a539f607f89223c
Pavel <p-olhovoi@bk.ru>
2024-01-19 12:49:49 +0100

    Change description for smoke test

--- cypress/e2e/topicmaps-elektromobilitaet.cy.js
+++ cypress/e2e/topicmaps-elektromobilitaet.cy.js
@@ -1,5 +1,5 @@
-describe("Smoke test topicmaps elektromobilitaet", () => {
-  it.only("First loading auto", () => {
+describe("Smoke test for topic maps with electric car charging stations", () => {
+  it("Map loads with interactive markers and key controls such as zoom in, zoom out, menu, info block, address entry fields", () => {
     cy.visit("https://wunda-geoportal.cismet.de/#/elektromobilitaet?title");
 
     cy.get(".leaflet-control-zoom-out").should("be.exist");
